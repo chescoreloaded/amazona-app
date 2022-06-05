3beb01ef79668e4ab64ce382206e7f72e7f458db
Francisco Roldan <ingfranciscoroldan@gmail.com>
2022-06-04 23:46:22 -0400

    ReactAdded

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data ={
+    products: [
+        {
+            _id:'1',
+            name:'Vikingo 1',
+            category: 'Figuras',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Mi Alegria',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Vikingo 2',
+            category: 'Dibujos',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 20,
+            brand: 'HashBro',
+            rating: 4,
+            numReviews: 9,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Vikingo 3',
+            category: 'Figuras',
+            image: '/images/p3.jpg',
+            price: 110,
+            countInStock: 0,
+            brand: 'Matel',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Vikingo 4',
+            category: 'Dibujos Animados',
+            image: '/images/p4.jpg',
+            price: 100, 
+            countInStock: 14,
+            brand: 'Mi Alegria',
+            rating: 3,
+            numReviews: 8,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Vikingo 5',
+            category: 'Figuras',
+            image: '/images/p5.jpg',
+            price: 80,
+            countInStock: 11,
+            brand: 'Troncoso',
+            rating: 2.5,
+            numReviews: 14,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Vikingo 6',
+            category: 'Figuras',
+            image: '/images/p1.jpg',
+            price: 90,
+            countInStock: 5,
+            brand: 'Mi Alegria',
+            rating: 3,
+            numReviews: 11,
+            description: 'High quality product'
+        }
+    ]
+};
+
+export default data; 
